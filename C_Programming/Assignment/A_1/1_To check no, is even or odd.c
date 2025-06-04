af35f27425fe62fb5df9026acab930ea07e56582
BharkarHariprasad <bharkarhariprasad007@gmail.com>
2025-06-04 14:45:10 +0530

    Add files via upload

---
+++ C_Programming/Assignment/A_1/1_To check no, is even or odd.c
@@ -0,0 +1,11 @@
+// To check whether a number is even or odd..
+
+void main(){
+	int a = 7;
+	if(a % 2 == 0){
+		printf("%d is Even", a);
+	}
+	else{
+		printf("%d is Odd", a);
+	}
+}
---
+++ C_Programming/Assignment/A_1/2_Given no, is palindrome or not.c
@@ -0,0 +1,18 @@
+// To check given three digit number is palindrome or not..
+
+void main(){
+	int num = 454;
+	int a1, a2, a3, q1, rev;
+	a1 = num % 10;
+	q1 = num / 10;  // To remove num last digit ( 6 ).
+	a2 = q1 % 10;
+	a3 = q1 / 10;
+	rev = a1 * 100 + a2 * 10 + a3;
+	
+	if(num == rev){
+		printf("%d is Pallindrome", num);
+	}
+	else{
+		printf("%d is not a Pallindrome", rev);
+	}
+}
